4c3e639e160bc734ffece675f9efc3eab09a4182
Kairnex <codexkairnex@gmail.com>
2025-02-21 21:51:53 +0530

    Update dev.py

--- Deva/modules/dev.py
+++ Deva/modules/dev.py
@@ -20,9 +20,9 @@ def allow_groups(update: Update, context: CallbackContext):
         update.effective_message.reply_text(f"Current state: {Deva.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
-        Deva.ALLOW_CHATS = True
-    elif args[0].lower() in ["yes", "on"]:
         Deva.ALLOW_CHATS = False
+    elif args[0].lower() in ["yes", "on"]:
+        Deva.ALLOW_CHATS = True
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
@@ -38,50 +38,45 @@ def leave(update: Update, context: CallbackContext):
         try:
             bot.leave_chat(int(chat_id))
         except TelegramError:
-            update.effective_message.reply_text(
-                "Beep boop, I could not leave that group(dunno why tho)."
-            )
+            update.effective_message.reply_text("I couldn't leave that group.")
             return
         with suppress(Unauthorized):
-            update.effective_message.reply_text("Beep boop, I left that soup!.")
+            update.effective_message.reply_text("I left that group!")
     else:
-        update.effective_message.reply_text("Send a valid chat ID")
+        update.effective_message.reply_text("Send a valid chat ID.")
 
 
 @dev_plus
 def gitpull(update: Update, context: CallbackContext):
-    sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart."
-    )
-    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
+    sent_msg = update.effective_message.reply_text("Pulling latest changes...")
 
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+    result = subprocess.run(["git", "pull"], capture_output=True, text=True)
+
+    sent_msg.edit_text(f"Git Pull Output:\n{result.stdout}\nRestarting in...")
 
     for i in reversed(range(5)):
-        sent_msg.edit_text(sent_msg_text + str(i + 1))
+        sent_msg.edit_text(f"Restarting in {i+1}...")
         sleep(1)
 
-    sent_msg.edit_text("Restarted.")
-
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
+    restart_bot()
 
 
 @dev_plus
 def restart(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one"
-    )
+    update.effective_message.reply_text("Restarting bot...")
 
-    os.system("restart.bat")
-    os.execv("start.bat", sys.argv)
+    restart_bot()
 
 
-LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
-GITPULL_HANDLER = CommandHandler("gitpull", gitpull, run_async=True)
-RESTART_HANDLER = CommandHandler("reboot", restart, run_async=True)
-ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups, run_async=True)
+def restart_bot():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
 
+
+LEAVE_HANDLER = CommandHandler("leave", leave)
+GITPULL_HANDLER = CommandHandler("gitpull", gitpull)
+RESTART_HANDLER = CommandHandler("reboot", restart)
+ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups)
 
 dispatcher.add_handler(ALLOWGROUPS_HANDLER)
 dispatcher.add_handler(LEAVE_HANDLER)
@@ -89,5 +84,4 @@ dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
 __mod_name__ = "Dᴇᴠ"
-
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
